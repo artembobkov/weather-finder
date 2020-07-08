6829d005918eb90c79aa0b4084276ce8f92fd264
Artem Bobkov <humantorch@mail.ru>
2020-07-08 14:58:47 +0300

    Updates

---
+++ precache-manifest.52df98a8f3f2409c1508085cbfc28ca8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdc3c99627ed82ebaccb9959a915d6cc",
+    "url": "/weather-finder/index.html"
+  },
+  {
+    "revision": "e9e61d6ebecf04318a7e",
+    "url": "/weather-finder/static/css/main.f615728c.chunk.css"
+  },
+  {
+    "revision": "6fa89b56b309c55a2dde",
+    "url": "/weather-finder/static/js/2.b66bd7d9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather-finder/static/js/2.b66bd7d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9e61d6ebecf04318a7e",
+    "url": "/weather-finder/static/js/main.31fe862e.chunk.js"
+  },
+  {
+    "revision": "82ea861833a341eaf660",
+    "url": "/weather-finder/static/js/runtime-main.81a178c6.js"
+  }
+]);
